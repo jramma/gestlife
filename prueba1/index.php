8c1f90ab7d41086f90d279784f3e32a12d842f45
jramma <jrmasip097@gmail.com>
2025-10-31 09:38:48 +0100

    pregunta 1

---
+++ prueba1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <h2>Iniciar Sesión</h2>
+        <form class="login-form" action="#" method="post">
+            <input type="text" name="usuario" placeholder="Usuario" required>
+            <input type="password" name="password" placeholder="Contraseña" required>
+            <button type="submit">Iniciar sesión</button>
+        </form>
+    </div>
+</body>
+
+</html>
